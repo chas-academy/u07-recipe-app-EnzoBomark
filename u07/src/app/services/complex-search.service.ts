294ed7c73c1fe5a377f95f861128716f7819e2d6
EnzoBomark <enzo.bomark@chasacademy.se>
2021-03-18 10:07:43 +0100

    Add filters to complex-search

--- u07/src/app/services/complex-search.service.ts
+++ u07/src/app/services/complex-search.service.ts
@@ -10,17 +10,40 @@ import{ GlobalConstants } from '../common/global-constants';
 
 export class ComplexSearchService {
   //RESIPE SEARCH//
-  recipesUrl:string = 'https://api.spoonacular.com/recipes/complexSearch';
+  API_URL:string = 'https://api.spoonacular.com/recipes/complexSearch';
   API_KEY:string = GlobalConstants.API_KEY;
-  number:string = '?number=10'
-  type:string = '&type=dessert'
 
-  //ADD TYPE SEARCH
-  //ADD SEARCH OPTIONS
+  //QUERYS//
+  NUMBER:string = '?number=100';
+
+  QUERY:string = '&query=';
+  queryValue:string = '';
+  query:string = `${this.QUERY}${this.queryValue}`;
+
+
+  //ADD type
+  TYPE:string = '&type=';
+  typeValue:string = '';
+  type:string = `${this.TYPE}${this.typeValue}`;
+
+  //ADD intolerances
+  INTOLERANCES:string = '&intolerances=';
+  intoleranceValue:string = '';
+  intolerances:string = `${this.INTOLERANCES}${this.intoleranceValue}`;
+
+  //ADD cuisine
+  CUISINE:string = '&cuisine=';
+  cuisineValue:string = '';
+  cuisine:string = `${this.CUISINE}${this.cuisineValue}`;
+
+  //ADD diet
+  DIET:string = '&diet=';
+  dietValue:string = '';
+  diet:string = `${this.DIET}${this.dietValue}`;
 
   constructor(private http: HttpClient) { }
 
   getComplexSearch():Observable<ComplexSearch[]> {
-    return this.http.get<ComplexSearch[]>(`${this.recipesUrl}${this.number}${this.type}${this.API_KEY}`);
+    return this.http.get<ComplexSearch[]>(`${this.API_URL}${this.NUMBER}${this.query}${this.type}${this.intolerances}${this.cuisine}${this.diet}${this.API_KEY}`);
   }
 }
